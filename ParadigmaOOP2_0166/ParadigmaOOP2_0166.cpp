bf8dab9e83dad2f70486f431bbc4e5e64ae09c7b
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-28 10:19:34 +0700

    menambahkan class turunan joko, inharitance dari class seseorang.

--- ParadigmaOOP2_0166/ParadigmaOOP2_0166.cpp
+++ ParadigmaOOP2_0166/ParadigmaOOP2_0166.cpp
@@ -4,9 +4,13 @@ using namespace std;
 class seseorang {
 public:
     virtual void pesan() = 0; // pure virtual function
-    virtual void pesan() { // virtual function biasa
-        cout << "Pesan dari seseorang" << endl;
-    }
+    //virtual void pesan() { // virtual function biasa
+    //    cout << "pesan dari seseorang" << endl;
+    //}
+};
+
+class joko : public seseorang {
+    
 };
 
 int main()
