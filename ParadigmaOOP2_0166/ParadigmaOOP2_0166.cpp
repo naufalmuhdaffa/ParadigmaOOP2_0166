b88c42e36b42a158845dd92d086f25e8421f5564
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-28 10:15:35 +0700

    menambahkan virtual function biasa

--- ParadigmaOOP2_0166/ParadigmaOOP2_0166.cpp
+++ ParadigmaOOP2_0166/ParadigmaOOP2_0166.cpp
@@ -4,7 +4,9 @@ using namespace std;
 class seseorang {
 public:
     virtual void pesan() = 0; // pure virtual function
-
+    virtual void pesan() { // virtual function biasa
+        cout << "Pesan dari seseorang" << endl;
+    }
 };
 
 int main()
