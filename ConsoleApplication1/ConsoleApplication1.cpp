2002e2fcadd40c24dd0754f06a8299d32244a898
naufalmuhdaffa <naufaldaffa878@gmail.com>
2024-05-28 10:33:23 +0700

    menambahkan class

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+//    class parent
+//    tambahkan final sesudah nama class
+//    untuk mencegah adanya overriding
+class baseClass {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
 int main()
 {
     
